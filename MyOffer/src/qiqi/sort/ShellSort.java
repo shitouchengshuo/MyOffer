a18eaf2328b419e036a7691d9799e6547aa0c6ab
shitouchengshuo <592197477@qq.com>
2018-10-30 11:01:07 +0800

    ppp

--- MyOffer/src/qiqi/sort/ShellSort.java
+++ MyOffer/src/qiqi/sort/ShellSort.java
@@ -18,7 +18,7 @@ public class ShellSort {
                 array[j + number] = key;
             }
             number = number / 2;
-        }
+        }//
     }
 
     public static void main(String[] args) {
