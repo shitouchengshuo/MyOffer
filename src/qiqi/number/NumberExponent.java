9f6ae4ebd4b8073f40d16bc603276c62eadc8dab
ll <592197477@qq.com>
2018-12-29 09:35:46 +0800

    不用加减乘除做加法

--- src/qiqi/number/NumberExponent.java
+++ src/qiqi/number/NumberExponent.java
@@ -1,7 +1,7 @@
 package qiqi.number;
 
 public class NumberExponent {
-    /**     * 实现函数double Power(double base, int exponent)，求base的exponent次方。
+    /** 实现函数double Power(double base, int exponent)，求base的exponent次方。
      *  不得使用库函数，同时不需要考虑大数问题。
      *
      * @param base     指次
