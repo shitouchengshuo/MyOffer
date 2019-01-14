e7fbeb35fa2fba00f15b22da952a2f4a9c974632
qiqi.zhao <qiqi.zhao@phicomm.com>
2019-01-14 09:33:45 +0800

    增加排序算法的解释

--- src/qiqi/sort/HeapSort.java
+++ src/qiqi/sort/HeapSort.java
@@ -7,39 +7,39 @@ public class HeapSort {
 
     private static void heapSort(int[] arr) {
         int len = arr.length -1;
-        for(int i = len/2; i >=0; i--){
-            heapAdjust(arr,i,len);//堆构造
+        for(int i = len / 2; i >= 0; i--){
+            heapAdjust(arr,i,len);  //堆构造
         }
-        while (len >=0){
-            swap(arr,0,len--); //先使用后自减，将堆顶元素与尾节点交换后，长度减1，尾元素最大
-            heapAdjust(arr,0,len); //再次对堆进行调整,从堆顶节点开始交换
+        while (len >= 0){
+            swap(arr,0, len--); //先使用后自减，将堆顶元素与尾节点交换后，长度减1，尾元素最大
+            heapAdjust(arr,0, len); //再次对堆进行调整,从堆顶节点开始交换
         }
     }
 
-    public static  void heapAdjust(int[] arr,int i,int len){
+    public static  void heapAdjust( int[] arr, int i, int len){
         int left,right,j ;//i指向子树的根节点
-        while((left = 2*i+1) <= len){//如果当前父节点有左节点（即有孩子节点，left为左节点）
+        while((left = 2 * i + 1) <= len){//如果当前父节点有左节点（即有孩子节点，left为左节点）
             right = left + 1;  //右节点为左节点+1
             j = left;   //j"指针指向左节点"
             //如果右节点大于左节点，下一个if语句比较arr[i]（父节点）与右节点
-            //这里j<len是在堆顶元素与尾节点交换后，保留排好序的最大值，即不再让尾节点参与交换（80...）
-            if( j < len &&arr[left] < arr[right]) {
+            //这里j<len是在堆顶元素与尾节点交换后，不再让尾节点参与交换（80...），以此来保留排好序的最大值
+            if( j < len && arr[left] < arr[right]) {
                 j = right;     //当前把"指针"指向右节点
             }
             //如果父节点小于孩子节点，将父节点与孩子节点交换
             // 这里：如果上面if为真，则arr[j]为右节点，如果为假arr[j]则为左节点
-            if(arr[i] < arr[j]){
-                swap(arr,i,j);
+            if( arr[i] < arr[j] ){
+                swap(arr, i, j);
             }
             else {
                 break; //说明根节点比孩子节点都大，直接跳出循环语句
             }
-            i = j; //1.堆顶元素80与第二个父节点元素20交换后，20还有子节点5,6元素进行比较
-                   //2.堆顶元素与尾节点交换后 调整是从堆顶开始，用于控制流程
+            i = j; //1.堆顶元素80与第二个父节点元素20交换后，20还有子节点5,6元素需要进行比较
+                   //2.堆顶元素与尾节点交换后，调整是从堆顶开始，用于控制流程
         }
     }
 
-    public static  void swap(int[] arr,int i,int len){
+    public static  void swap(int[] arr, int i, int len){
         int temp = arr[i];
         arr[i] = arr[len];
         arr[len] = temp;
--- src/qiqi/sort/ShellSort.java
+++ src/qiqi/sort/ShellSort.java
@@ -1,5 +1,32 @@
 package qiqi.sort;
 
+/**
+ * 希尔排序的实质就是分组插入排序，该方法又称缩小增量排序，因DL．Shell于1959年提出而得名。
+ * 该方法的基本思想是：先将整个待排元素序列分割成若干个子序列（由相隔某个“增量”的元素组成的）分别进行直接插入排序，
+ * 然后依次缩减增量再进行排序，待整个序列中的元素基本有序（增量足够小）时，再对全体元素进行一次直接插入排序。
+ * 因为直接插入排序在元素基本有序的情况下（接近最好情况），效率是很高的，因此希尔排序在时间效率上比前两种方法有较大提高。
+ * 时间复杂度：受增量序列影响，当增量k = 2^x时，O(n^2)，当增量 k = 3x+1时，O(n^(3/2))
+ *
+ * 第一次分成5组   gap = 10/2=5
+ *   49  38  65  97  26  13  27  49  55  4
+ *   1A                  1B
+ *       2A                  2B
+ *           3A                  3B
+ *               4A                  4B
+ *                   5A                   5B
+ *
+ * 第二次 分成2组  gap = 5/2 =2
+ *   13  27  49  55  4  49  38  65  97  26
+ *   1A      1B      1C     1D      1E
+ *       2A      2B     2C      2D      2E
+ *
+ * 第三次  gap = 2/2 = 1
+ *    4  26  13  27  38  49  49  55  97  65
+ *   1A  1B  1C  1D  1E  1F  1G  1H  1I  1J
+ *
+ * 第四次  gap = 1/2 = 0
+ *    4  13  26  27  38  49  49  55  65  97
+  */
 public class ShellSort {
 
     public static void shellSort(int[] array){
@@ -7,18 +34,18 @@ public class ShellSort {
             return;
         }
         int number = array.length / 2;
-        while (number >= 1) {
+        while (number >= 1){
             for (int i = number; i < array.length; i++){
                 int key = array[i];
                 int j = i - number;
-                while (j >= 0 && array[j] > key){
-                    array[j + number] = array[j];
+                while ( j >= 0 && array[j] > key){
+                    array[j+number] = array[j];
                     j = j - number;
                 }
-                array[j + number] = key;
+                array[j+number] = key;
             }
             number = number / 2;
-        }//
+        }
     }
 
     public static void main(String[] args) {
